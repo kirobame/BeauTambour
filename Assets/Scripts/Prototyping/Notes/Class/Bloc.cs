1f222c5ee323f88a8cb61a1134f1d3ee4ca4ac3e
Hugo Carré <h.carre@rubika-edu.com>
2020-10-12 17:58:27 +0200

    BlocNoteMatch#01 Class Note and Bloc, match logic between notes and blocks

---
+++ Assets/Scripts/Prototyping/Notes/Class/Bloc.cs
@@ -0,0 +1,37 @@
+﻿using Orion.Prototyping;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Bloc : Tilable, IBootable
+{
+    [SerializeField]private Shape shape;
+
+    public Shape Shape => shape;
+
+    public void BootUp()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void ShutDown()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*System.Random rand = new System.Random(System.DateTime.Now.Second);
+        Array shapes = Enum.GetValues(typeof(Shape));
+        shape = (Shape)shapes.GetValue(rand.Next(shapes.Length));*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Prototyping/Notes/Class/Note.cs
@@ -0,0 +1,48 @@
+﻿using Orion;
+using Orion.Prototyping;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Note : Tilable, IResolvable
+{
+    private int priority;
+    [SerializeField]private Shape shapesMatched;
+    public int Priority { get => priority; set => priority = value; }
+
+    public void Resolve()
+    {
+        PlayArea playArea = Repository.Get<PlayArea>();
+        for (int x = Tile.Index.x + 1; x < playArea.Size.x; x++)
+        {
+            if (playArea[x, Tile.Index.y][TilableType.NoteRecipient].Any())
+            {
+                Bloc block = playArea[x, Tile.Index.y][TilableType.NoteRecipient].First() as Bloc;
+                if ((block.Shape & shapesMatched) == block.Shape)
+                {
+                    Debug.Log("MATCH!");
+                }
+                else
+                {
+                    Debug.Log("NO MATCH!");
+                }
+                return;
+            }
+        }
+        Debug.Log("NOTHING!");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        RoundHandler roundHandler =  Repository.Get<RoundHandler>();
+        roundHandler.TryEnqueue(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
