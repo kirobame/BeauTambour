8b3984f7071d646fde556643f90440d92c243d0b
Hugo Carré <hcarre201@sfr.fr>
2020-12-23 01:53:11 +0100

    RW-Drawing - #01 Stick indicator movement

---
+++ Assets/Scripts/Drawing/StickIndicatorBehavior.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Flux;
+using Event = Flux.Event;
+using Deprecated;
+
+namespace BeauTambour
+{
+    public class StickIndicatorBehavior : MonoBehaviour
+    {
+        [SerializeField] private float smoothing = 1.0f;
+
+        private Vector2 velocity;
+
+        private void Awake()
+        {
+            Event.Register<Vector2>(StickIndicatorOperation.EventType.OnUpdate, OnUpdate);
+            Event.Register<Vector2>(StickIndicatorOperation.EventType.OnEnd, OnEnd);
+        }
+
+        private void Place(Vector2 input, bool smooth)
+        {
+            var stickIndicator = Repository.GetSingle<Transform>(Reference.StickIndicator);
+            if (smooth)
+            {
+                stickIndicator.localPosition = Vector2.SmoothDamp(stickIndicator.localPosition, input, ref velocity, smoothing);
+            }
+            else
+            {
+                stickIndicator.localPosition = input;
+            }
+        }
+
+        private void OnUpdate(Vector2 input)
+        {
+            Place(input, true);
+        }
+
+        private void OnEnd(Vector2 input)
+        {
+            Place(input, false);
+        } 
+
+    }
+}
---
+++ Assets/Scripts/Drawing/StickIndicatorOperation.cs
@@ -0,0 +1,51 @@
+﻿using Flux;
+using System;
+using UnityEngine;
+
+namespace BeauTambour
+{
+    [CreateAssetMenu(fileName = "NewCursorOperation", menuName = "Beau Tambour/Inputs/Operations/Cursor")]
+    public class StickIndicatorOperation : SingleOperation
+    {
+        public enum EventType
+        {
+            OnUpdate,
+            OnEnd
+        }
+
+        public override void Initialize(MonoBehaviour hook)
+        {
+            base.Initialize(hook);
+            Flux.Event.Open<Vector2>(EventType.OnUpdate);
+            Flux.Event.Open<Vector2>(EventType.OnEnd);
+        }
+
+        public override void OnUpdate(EventArgs inArgs)
+        {
+            if (!(inArgs is Vector2EventArgs vector2EventArgs)) return;
+            Flux.Event.Call(EventType.OnUpdate,vector2EventArgs.value);
+        }
+        public override void OnEnd(EventArgs inArgs)
+        {
+            Flux.Event.Call(EventType.OnEnd, Vector2.zero);
+        }
+
+        // Use Game state
+        //
+        // SELECTION
+        //  Check cursor position comparing to emotions position (lerp, grow the section)
+        //      IF catch zone magnetize cursor
+        //          THEN IF validation Input -> drawing phase 
+        //      ELSE
+        //          demagnetize cursor and desactivate Inputs
+        // DRAWING
+        //  
+        //
+        //
+        //
+        //
+        //
+
+
+    }
+}
