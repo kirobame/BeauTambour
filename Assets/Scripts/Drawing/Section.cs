952be246991261582b3832cd623ab627d9dbe4ae
Hugo Carré <hcarre201@sfr.fr>
2020-12-23 21:29:28 +0100

    RW-Drawing - #03 Magnetization working

--- Assets/Scripts/Drawing/Section.cs
+++ Assets/Scripts/Drawing/Section.cs
@@ -9,18 +9,29 @@ namespace BeauTambour
     public class Section
     {
         [SerializeField] private Transform anchor;
-        [SerializeField] private Vector2 dir;
-        [SerializeField] private float errorOffset;// 45 degrees max per emotion (max 0.5 as dotResult)
+        [SerializeField] private float errorOffset = 0.2f;// 45 degrees max per emotion (max 0.5 as dotResult)
         [SerializeField] private Emotion emotion;
+        [SerializeField] private float catchZonedistance = 0.3f;
 
         private float maxScale = 1.0f;
         private float minScale = 0.5f;
+        
         private float velocity;
 
-        public bool IsInSection(Vector2 position)
+        public Emotion Emotion
         {
-            float dotResult = Vector2.Dot(position.normalized, dir);
-            if (dotResult >= errorOffset)
+            get
+            {
+                return emotion;
+            }
+        }
+
+        public Transform Anchor { get => anchor;}
+
+        public bool IsInSection(Vector2 input)
+        {
+            float dotResult = Vector2.Dot(input.normalized, anchor.localPosition);
+            if (dotResult >= 1 - errorOffset)
             {
                 Debug.Log(emotion);
                 return true;
@@ -28,20 +39,25 @@ namespace BeauTambour
             return false;
         }
 
-        public float GetDistanceWithAnchor(Vector2 position)
+        public float GetDistanceWithAnchor(Vector2 input)
         {
-            Vector2 diff = dir - position;
-            return Mathf.Clamp01(diff.magnitude);
+            Vector2 diff = (Vector2)anchor.localPosition - input;
+            return diff.magnitude;
         }
 
-        public void ScaleAnchor(Vector2 position)
+        public void ScaleAnchor(Vector2 input)
         {
-            anchor.localScale = new Vector2(1, 1) * Mathf.Lerp(maxScale,minScale,GetDistanceWithAnchor(position));
+            anchor.localScale = new Vector2(1, 1) * Mathf.Lerp(maxScale,minScale,GetDistanceWithAnchor(input));
         }
 
         public void SmoothScaleAnchorReset()
         {
-            anchor.localScale = new Vector2(1, 1) * Mathf.SmoothDamp(anchor.localScale.x, minScale, ref velocity, 0.1f);
+            anchor.localScale = new Vector2(1, 1) * Mathf.SmoothDamp(anchor.localScale.x, minScale, ref velocity, 0.5f);
+        }
+
+        public bool IsInCatchZone(Vector2 input)
+        {
+            return GetDistanceWithAnchor(input) <= catchZonedistance?true:false;
         }
     }
 }
--- Assets/Scripts/Drawing/StickIndicatorBehavior.cs
+++ Assets/Scripts/Drawing/StickIndicatorBehavior.cs
@@ -4,28 +4,92 @@ using UnityEngine;
 using Flux;
 using Event = Flux.Event;
 using Deprecated;
+using System;
 
 namespace BeauTambour
 {
     public class StickIndicatorBehavior : MonoBehaviour
     {  
+        public enum EventType
+        {
+            OnMagnetize,
+            OnUnMagnetize,
+        }
+
         [SerializeField] private List<Section> sections;
-        [SerializeField] private float smoothing = 1.0f;
+        [SerializeField] private float smoothing = 0.1f;
 
         private Vector2 velocity;
         private Transform stickIndicator;
+        private Vector2 cursorPos;
+        private Section magnetizedSection;
 
         private void Awake()
         {
-            
+            magnetizedSection = null;
+            Event.Open(EventType.OnMagnetize);
+            Event.Open(EventType.OnUnMagnetize);
+
             Event.Register<Vector2>(StickIndicatorOperation.EventType.OnUpdate, OnUpdate);
             Event.Register<Vector2>(StickIndicatorOperation.EventType.OnEnd, OnEnd);
         }
 
+        private void Start()
+        {
+            stickIndicator = Repository.GetSingle<Transform>(Reference.StickIndicator);
+        }
+
+        private void OnUpdate(Vector2 input)
+        {
+            if (magnetizedSection != null)
+            {
+                MagnetizeCursorToSection();
+                if (!magnetizedSection.IsInCatchZone(input))
+                {
+                    magnetizedSection = null;
+                    Event.Call(EventType.OnUnMagnetize);
+                    return;
+                }
+            }
+            else
+            {
+                Place(input, true);
+                cursorPos = stickIndicator.localPosition;
+            }            
+            foreach (Section section in sections)
+            {
+                if (section.IsInSection(cursorPos))
+                {
+                    section.ScaleAnchor(cursorPos);
+                    if (magnetizedSection == null && section.IsInCatchZone(cursorPos))
+                    {
+                        magnetizedSection = section;
+                        Event.Call(EventType.OnMagnetize);
+                    }
+                }
+                else
+                {
+                    section.SmoothScaleAnchorReset();
+                }
+            }
+        }
+
+        private void OnEnd(Vector2 input)
+        {
+            if (magnetizedSection != null)
+            {
+                magnetizedSection = null;
+            }
+            Place(input, false);
+            cursorPos = stickIndicator.localPosition;
+            foreach (Section section in sections)
+            {
+                section.ScaleAnchor(cursorPos);
+            }
+        }
 
         private void Place(Vector2 input, bool smooth)
         {
-            stickIndicator = Repository.GetSingle<Transform>(Reference.StickIndicator);
             if (smooth)
             {
                 stickIndicator.localPosition = Vector2.SmoothDamp(stickIndicator.localPosition, input, ref velocity, smoothing);
@@ -36,44 +100,22 @@ namespace BeauTambour
             }
         }
 
-        private void OnUpdate(Vector2 input)
+        private void MagnetizeCursorToSection()
         {
-            Place(input, true);
-            foreach (Section section in sections)
+            Place(magnetizedSection.Anchor.localPosition, true);
+            cursorPos = stickIndicator.localPosition;
+            magnetizedSection.ScaleAnchor(magnetizedSection.Anchor.localPosition);
+        }
+
+#if UNITY_EDITOR
+        private void OnDrawGizmos()
+        {
+            foreach (var item in sections)
             {
-                if (section.IsInSection(input))
-                {
-                    section.ScaleAnchor(input);
-                }
-                else
-                {
-                    section.SmoothScaleAnchorReset();
-                }
-                //Distance
-                //Angle
-                //Actualise section
+                Gizmos.color = Color.black;
+                Gizmos.DrawWireSphere(item.Anchor.position, 0.3f);
             }
         }
-
-        private void OnEnd(Vector2 input)
-        {
-            Place(input, false);
-        }
-
-        // Use Game state
-        //
-        // SELECTION
-        //  Check cursor position comparing to emotions position (lerp, grow the section)
-        //      IF catch zone magnetize cursor
-        //          THEN IF validation Input -> drawing phase 
-        //      ELSE
-        //          demagnetize cursor and desactivate Inputs
-        // DRAWING
-        //  
-        //
-        //
-        //
-        //
-        //
+#endif
     }
 }
