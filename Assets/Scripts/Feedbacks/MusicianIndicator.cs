a112015e14102ed6a1a9dc62665fd180b02862e4
Hugo Carré <hcarre201@sfr.fr>
2021-01-04 17:13:56 +0100

    OnSelectionFeedbacks - #05 Selected musician icon while drawing

---
+++ Assets/Scripts/Feedbacks/MusicianIndicator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Flux;
+using Event = Flux.Event;
+
+namespace BeauTambour
+{
+    public class MusicianIndicator : MonoBehaviour
+    {
+        [SerializeField] private MusicianIconRegistry icons;
+        [SerializeField] private SpriteRenderer musicianIndicator;
+
+        private void Start()
+        {
+            Event.Register($"{PhaseCategory.EmotionDrawing}.{PhaseCallback.Start}",SetMusicianSprite);
+        }
+
+        public void SetMusicianSprite()
+        {
+            Sprite sprite;
+            if(icons.TryGet(GameState.Note.speaker.Actor, out sprite))
+            {
+                musicianIndicator.sprite = sprite;
+            }
+            else
+            {
+                Debug.LogWarning($"{GameState.Note.speaker.Actor}'s icon not found in registry.");
+            }
+        }
+    }
+}
