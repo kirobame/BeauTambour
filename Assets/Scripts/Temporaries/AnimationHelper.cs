c766b15bc5ab0c500d4bd9b7f5d672516285abd9
RisuNiku <l.marchal@rubika-edu.com>
2021-01-06 19:10:31 +0100

    CharacterAnimation - Add musicians rough emote animations

---
+++ Assets/Scripts/Temporaries/AnimationHelper.cs
@@ -0,0 +1,44 @@
+﻿using Flux;
+using UnityEngine;
+
+namespace BeauTambour
+{
+    public class AnimationHelper : MonoBehaviour
+    {
+        private enum Trigger
+        {
+            Joie,
+            Anticipation,
+            Colere,
+            Confiance,
+            Degout,
+            Peur,
+            Tristesse,
+            Surprise
+        }
+        
+        [ContextMenuItem("Execute", "Execute")]
+        [SerializeField] private RuntimeCharacter character;
+
+        [ContextMenuItem("Execute", "Execute")]
+        [SerializeField] private PoolableAnimation vfx;
+        
+        [ContextMenuItem("Execute", "Execute")]
+        [Space, SerializeField] private Animator animator;
+
+        [ContextMenuItem("Execute", "Execute")] 
+        [SerializeField] private Trigger trigger;
+
+        public void Execute()
+        {
+            var vfxPool = Repository.GetSingle<AnimationPool>(References.AnimationPool);
+            var poolableVfx = vfxPool.RequestSingle(vfx);
+
+            poolableVfx.transform.parent = character.HeadSocket.Attach;
+            poolableVfx.transform.localPosition = Vector3.zero;
+            
+            poolableVfx.SetTrigger("Play");
+            animator.SetTrigger($"TriggerEmote{trigger}");
+        }
+    }
+}
